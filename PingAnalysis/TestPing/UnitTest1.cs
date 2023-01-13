c0f25e3e5a1e15550d8289a0c5a5772ce70cc37c
ashoka bantumilli <ashoka.bantumilli@gmail.com>
2023-01-13 09:57:19 +0530

    Basic test case added

--- PingAnalysis/TestPing/UnitTest1.cs
+++ PingAnalysis/TestPing/UnitTest1.cs
@@ -1,3 +1,6 @@
+using PingAnalysis;
+using System.Windows;
+
 namespace TestPing
 {
     [TestClass]
@@ -6,6 +9,29 @@ namespace TestPing
         [TestMethod]
         public void TestMethod1()
         {
+            Thread thread = new Thread(() =>
+            {
+                int count = 0;
+                var application = new App();
+
+                while (true)
+                {
+                    application.Dispatcher.InvokeAsync(() =>
+                    {
+                        var vm = new MainWindow();
+                        vm.Show();
+                    }, System.Windows.Threading.DispatcherPriority.ApplicationIdle);
+                    Thread.Sleep(1000);
+                    if (count++ == 5)
+                    {
+                        break;
+                    }
+                }
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
         }
     }
 }
