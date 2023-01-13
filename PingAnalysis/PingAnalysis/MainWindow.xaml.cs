9a6e35e93abae6e5d29788036a9c0ea788ffb1f0
ashoka bantumilli <ashoka.bantumilli@gmail.com>
2023-01-13 08:50:20 +0530

    Minute wise aggregation - parse and store ping result in to a file on current date

--- PingAnalysis/PingAnalysis/MainWindow.xaml.cs
+++ PingAnalysis/PingAnalysis/MainWindow.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.Tracing;
 using System.DirectoryServices.ActiveDirectory;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup.Localizer;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -25,8 +29,38 @@ namespace PingAnalysis
     {
         bool syncping = false;
         private string iporhost;
+        Dictionary<string, List<int> > pingresult = new Dictionary<string, List<int> >();
         public MainWindow()
         {
+            Thread tid = new Thread(new ThreadStart(() => {
+                string fileName = @"ip_" + DateTime.Now.Date.ToString("yyyyMMdd") + ".txt";
+                int minuteCount = 0;
+                while (true)
+                {
+                    Thread.Sleep(60000);
+                    minuteCount++;
+                    foreach (var host in pingresult)
+                    {
+                        var times = host.Value;
+                        times.Sort();
+                        using (StreamWriter sw = File.AppendText(fileName))
+                        {
+                            sw.WriteLine("Host = " + host.Key + " " + "Min = " + times[0].ToString() + " " 
+                                + "Average = " + times[times.Count/2].ToString() + " " 
+                                + "Max = " + times[times.Count - 1].ToString());
+                        }
+
+                        Dispatcher.Invoke(() =>
+                        {
+                            this.button.Content = times[0].ToString();
+                            this.button1.Content = times[times.Count / 2].ToString();
+                            this.button2.Content = times[times.Count - 1].ToString();
+                            this.button3.Content = minuteCount;
+                        });
+                    }
+                }
+            }));
+            tid.Start();
             InitializeComponent();
         }
 
@@ -44,7 +78,7 @@ namespace PingAnalysis
                         StartInfo = new ProcessStartInfo
                         {
                             FileName = "C:\\Windows\\System32\\ping.exe",
-                            Arguments = iporhost + " -n 1",
+                            Arguments = iporhost + " -n 1 -4",
                             UseShellExecute = false,
                             RedirectStandardOutput = true,
                             CreateNoWindow = true
@@ -59,6 +93,47 @@ namespace PingAnalysis
                                 while (!process.StandardOutput.EndOfStream)
                                 {
                                     var output = process.StandardOutput.ReadLine();
+                                    if (output != null && output.Contains("Reply from"))
+                                    {
+                                        var tokens = output.Split(' ');
+                                        foreach(var token in tokens)
+                                        {
+                                            if (token.Contains("time"))
+                                            {
+                                                if (token.Contains("="))
+                                                {
+                                                    var parse = token.Split("=");
+                                                    try
+                                                    {
+                                                        if (pingresult[tokens[2]] == null)
+                                                        {
+                                                            pingresult[tokens[2]] = new List<int>();
+                                                        }
+                                                    }
+                                                    catch (Exception ex) 
+                                                    {
+                                                        pingresult[tokens[2]] = new List<int>();
+                                                    }
+                                                    pingresult[tokens[2]].Add(Int32.Parse(parse[1].ToString().TrimEnd('s').TrimEnd('m')));
+                                                }
+                                                else if (token.Contains("<")) {
+                                                    var parse = token.Split("<");
+                                                    try
+                                                    {
+                                                        if (pingresult[tokens[2]] == null)
+                                                        {
+                                                            pingresult[tokens[2]] = new List<int>();
+                                                        }
+                                                    }
+                                                    catch (Exception ex)
+                                                    {
+                                                        pingresult[tokens[2]] = new List<int>();
+                                                    }
+                                                    pingresult[tokens[2]].Add(Int32.Parse(parse[1].ToString().TrimEnd('s').TrimEnd('m')));
+                                                }
+                                            }
+                                        }
+                                    }
                                     Dispatcher.Invoke(() => {
                                         this.listView.Items.Add(output);
                                     });
